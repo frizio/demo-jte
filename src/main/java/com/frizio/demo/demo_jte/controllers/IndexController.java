59c8a386814f5b0f36a5a7a2442961e785e11a96
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2025-02-01 23:39:49 +0100

    first commit jte

---
+++ src/main/java/com/frizio/demo/demo_jte/controllers/IndexController.java
@@ -0,0 +1,16 @@
+package com.frizio.demo.demo_jte.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+
+        return "index";
+    }
+
+}
---
+++ src/test/java/com/frizio/demo/demo_jte/DemoJteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frizio.demo.demo_jte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
