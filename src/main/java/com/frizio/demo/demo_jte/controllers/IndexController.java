e464f1eb5cb2a3211c90ef6de3dfb06bc8a9d286
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2025-02-02 23:23:36 +0100

    Template + layout

--- src/main/java/com/frizio/demo/demo_jte/controllers/IndexController.java
+++ src/main/java/com/frizio/demo/demo_jte/controllers/IndexController.java
@@ -4,13 +4,31 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 public class IndexController {
 
     @GetMapping("/")
     public String home(Model model) {
+        model.addAttribute("username", "John Doe");
+        return "pages/home";
+    }
 
-        return "index";
+    @GetMapping("/team")
+    public String team(Model model) {
+        List<String> teamMembers = Arrays.asList("Alice", "Bob", "Charlie", "David");
+        model.addAttribute("teamMembers", teamMembers);
+        return "pages/team";
+    }
+
+    @GetMapping("/projects")
+    public String projects(Model model) {
+        List<String> projects = List.of("Project 1", "Project 2", "Project 3");
+        model.addAttribute("username", "John Doe");
+        model.addAttribute("projects",projects);
+        return "pages/projects";
     }
 
 }
